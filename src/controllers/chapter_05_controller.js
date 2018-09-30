46fe34f4207007e4cd492b95b2e50a1cb6945aa1
Javan Makhmali <javan@javan.us>
2018-09-30 12:23:32 -0400

    Chapter 5: Putting it together

---
+++ src/controllers/chapter_05_controller.js
@@ -0,0 +1,59 @@
+import { Canvas, Color, Matrix, Position, Ray, Sphere } from "../models"
+import { createElementForCanvas, nextFrame, nextIdle } from "../helpers"
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "preview" ]
+
+  connect() {
+    this.render()
+  }
+
+  async render() {
+    await nextIdle()
+    const element = createElementForCanvas(this.canvas)
+
+    await nextFrame()
+    this.previewTarget.innerHTML = ""
+    this.previewTarget.appendChild(element)
+  }
+
+  async download(event) {
+    const blob = this.canvas.toPPM().toBlob()
+    const url = URL.createObjectURL(blob)
+    event.currentTarget.href = url
+
+    await nextFrame()
+    URL.revokeObjectURL(url)
+  }
+
+  get canvas() {
+    const rayOrigin = Position.point(0, 0, -5)
+    const sphere = new Sphere
+
+    const wallZ = 10
+    const wallSize = 7.0
+
+    const canvasSize = 150
+    const pixelSize = wallSize / canvasSize
+    const halfSize = wallSize / 2
+
+    const canvas = new Canvas(canvasSize, canvasSize)
+    const color = Color.of(0, 0, 1)
+
+    for (let y = 0; y < canvasSize; y++) {
+      const worldY = halfSize - pixelSize * y
+      for (let x = 0; x < canvasSize; x++) {
+        const worldX = -halfSize + pixelSize * x
+        const position = Position.point(worldX, worldY, wallZ)
+        const ray = new Ray(rayOrigin, position.subtract(rayOrigin))
+        const { hit } = ray.intersect(sphere)
+        if (hit) {
+          canvas.writePixel(x, y, color)
+        }
+      }
+    }
+
+    return canvas
+  }
+}
