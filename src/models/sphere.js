24de84066225da9b93b8481d3757a976804e019b
Javan Makhmali <javan@javan.us>
2018-10-06 12:38:21 -0400

    Chapter 6: Surface Normals

--- src/models/sphere.js
+++ src/models/sphere.js
@@ -1,7 +1,15 @@
 import { Matrix } from "./matrix"
+import { Position } from "./position"
 
 export class Sphere {
   constructor() {
     this.transform = Matrix.identity
+  }
+
+  normalAt(worldPoint) {
+    const objectPoint = this.transform.inverse.multiplyBy(worldPoint)
+    const objectNormal = objectPoint.subtract(Position.point(0, 0, 0))
+    const [ x, y, z ] = this.transform.inverse.transpose.multiplyBy(objectNormal)
+    return Position.vector(x, y, z).normalize
   }
 }
--- test/spheres_test.js
+++ test/spheres_test.js
@@ -81,3 +81,47 @@ test("intersecting a translated sphere with a ray", t => {
   const xs = r.intersect(s)
   t.is(xs.length, 0)
 })
+
+test("the normal on a sphere at a point on the x axis", t => {
+  const s = new Sphere
+  const n = s.normalAt(Position.point(1, 0, 0))
+  t.deepEqual(n, Position.vector(1, 0, 0))
+})
+
+test("the normal on a sphere at a point on the y axis", t => {
+  const s = new Sphere
+  const n = s.normalAt(Position.point(0, 1, 0))
+  t.deepEqual(n, Position.vector(0, 1, 0))
+})
+
+test("the normal on a sphere at a point on the z axis", t => {
+  const s = new Sphere
+  const n = s.normalAt(Position.point(0, 0, 1))
+  t.deepEqual(n, Position.vector(0, 0, 1))
+})
+
+test("the normal on a sphere at a non-axial point", t => {
+  const s = new Sphere
+  const n = s.normalAt(Position.point(Math.sqrt(3) / 3, Math.sqrt(3) / 3, Math.sqrt(3) / 3))
+  t.deepEqual(n, Position.vector(Math.sqrt(3) / 3, Math.sqrt(3) / 3, Math.sqrt(3) / 3))
+})
+
+test("the normal is a normalized vector", t => {
+  const s = new Sphere
+  const n = s.normalAt(Position.point(Math.sqrt(3) / 3, Math.sqrt(3) / 3, Math.sqrt(3) / 3))
+  t.deepEqual(n, n.normalize)
+})
+
+test("computing the normal on a translated sphere", t => {
+  const s = new Sphere
+  s.transform = Matrix.translation(0, 1, 0)
+  const n = s.normalAt(Position.point(0, 1.70711, -0.70711))
+  t.deepEqual(n.fixed, Position.vector(0, 0.70711, -0.70711))
+})
+
+test("computing the normal on a scaled sphere", t => {
+  const s = new Sphere
+  s.transform = Matrix.scaling(1, 0.5, 1)
+  const n = s.normalAt(Position.point(0, Math.sqrt(2) / 2, -Math.sqrt(2) / 2))
+  t.deepEqual(n.fixed, Position.vector(0, 0.97014, -0.24254))
+})
