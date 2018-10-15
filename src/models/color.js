e2322d47bb31552362cd5d3eccaa1e4c3e05bd9c
Javan Makhmali <javan@javan.us>
2018-10-15 10:16:48 -0400

    Remove unused constructor

--- src/models/color.js
+++ src/models/color.js
@@ -1,10 +1,6 @@
 import { Tuple } from "./tuple"
 
 export class Color extends Tuple {
-  constructor(red, green, blue) {
-    super(red, green, blue)
-  }
-
   get red()   { return this[0] }
   get green() { return this[1] }
   get blue()  { return this[2] }
