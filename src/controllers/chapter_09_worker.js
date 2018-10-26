e71ae57c1785ea577af1e95bd213c333151fefdc
Javan Makhmali <javan@javan.us>
2018-10-26 09:10:50 -0400

    Chapter 9: Putting more of it together

--- src/controllers/chapter_09_worker.js
+++ src/controllers/chapter_09_worker.js
@@ -48,36 +48,77 @@ class Scene {
     return Camera.create({
       hsize: this.width,
       vsize: this.height,
-      view: Math.PI / 3,
+      view: Math.PI / 1.5,
       transform: Matrix.viewTransform(
-        Point(0, 1, -5),
-        Point(0, 1, 0),
+        Point(0, 2, -5),
+        Point(0, 2, 0),
         Vector(0, 1, 0)
       )
     })
   }
 
   get world() {
-    const world = World.of(
-      this.floor,
-      this.sphere,
-    )
-    world.light = new PointLight(Point(-10, 10, -10), Color.WHITE)
+    const world = World.of(...this.walls, ...this.shapes)
+    world.light = new PointLight(Point(5, 1, -6), Color.of(1.6, 1.6, 1.6))
     return world
   }
 
-  get floor() {
-    return Plane.create({
-      color: Color.of(1, 0.9, 0.9)
-    })
+  get walls() {
+    const material = {
+      color: Color.of(1, 0.9, 0.9),
+      ambient: 0.05,
+      diffuse: 0.6,
+      specular: 0,
+    }
+    return [
+      Plane.create({
+        ...material,
+        diffuse: 1.2,
+        ambient: 0.1,
+      }),
+      Plane.create({
+        ...material,
+        transform: Matrix.translation(0, 0, 10)
+          .multiplyBy( Matrix.rotationX(Math.PI / 2) )
+      }),
+      Plane.create({
+        ...material,
+        transform: Matrix.translation(-8, 0, 10)
+          .multiplyBy( Matrix.rotationY(90/180 * -Math.PI) )
+          .multiplyBy( Matrix.rotationX(Math.PI / 2) )
+      })
+    ]
   }
 
-  get sphere() {
-    return Sphere.create({
-      color: Color.of(0.1, 1, 0.5),
-      diffuse: 0.7,
-      specular: 0.3,
-      transform: Matrix.translation(-0.5, 1, 0.5)
-    })
+
+  get shapes() {
+    return [
+      Sphere.create({
+        color: Color.of(0.1, 1, 0.5),
+        diffuse: 0.7,
+        specular: 0.3,
+        transform: Matrix.translation(1.5, 0, -1)
+          .multiplyBy( Matrix.rotationZ(Math.PI / 2) )
+          .multiplyBy( Matrix.scaling(0.4, 2.2, 1.8) )
+      }),
+      Sphere.create({
+        color: Color.of(0.1, 1, 0.5),
+        diffuse: 0.5,
+        specular: 0.3,
+        transform: Matrix.translation(1.5, 0.4, -1)
+          .multiplyBy( Matrix.rotationZ(Math.PI / 2) )
+          .multiplyBy( Matrix.rotationX(Math.PI / 4) )
+          .multiplyBy( Matrix.scaling(1.6, 0.2, 1.6) )
+      }),
+      Sphere.create({
+        color: Color.of(0.1, 1, 0.5),
+        diffuse: 0.5,
+        specular: 0.3,
+        transform: Matrix.translation(1.5, 0.4, -1)
+          .multiplyBy( Matrix.rotationZ(Math.PI / 2) )
+          .multiplyBy( Matrix.rotationX(-Math.PI / 3) )
+          .multiplyBy( Matrix.scaling(5, 0.2, 1.6) )
+      })
+    ]
   }
 }
